219e2771966860c32b3d361981988e1d36960aae
zwyatt <zwyatt.work@gmail.com>
2023-06-11 19:41:43 -0400

    just started work on the Question model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -97,23 +97,27 @@ class Enrollment(models.Model):
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
+    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course >(course? don't you mean lesson?)
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
     # Foreign key to lesson
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     # question text
+    text = models.CharField(null=False, max_length=200)
     # question grade/mark
+    mark = models.IntegerField(default=0)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    # >do questions get partial marks? should mark be a float? when do we call this?
+    def is_correct(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 
 #  <HINT> Create a Choice Model with:
